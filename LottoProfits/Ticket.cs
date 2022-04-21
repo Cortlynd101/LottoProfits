25ed66b0addd5a9a8a0a4630b530621d4a9573a3
Cortlynd101 <CortlyndCox@gmail.com>
2022-04-21 10:33:10 -0600

    Added test cases, basis for the program.

---
+++ LottoProfits/Ticket.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LottoProfits
+{
+    public class Ticket
+    {
+        public int[] ticketNumbers { get; set; }
+        public Ticket()
+        {
+            ticketNumbers = new int[6];
+        }
+
+        public Ticket createTicket(Ticket ticket)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool checkDuplicity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool checkNumberIsInRange(int p0, int p1)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
